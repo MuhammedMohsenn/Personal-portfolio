4cbe4fa9029c8121e771537b8e578d4ed12ed954
Mohamed Mohsen <muhammedmohsen8695@gmail.com>
2023-09-12 21:55:06 +0300

    uploading project

---
+++ lib/data.ts
@@ -0,0 +1,81 @@
+import personalPortfolio from "@/public/nextjs-portfolio.png";
+import shoppingHub from "@/public/shopping-hub-ecommerce.png";
+import foodeRecipeFinder from "@/public/Foode-nextjs.png";
+import banking from "@/public/Banking-website.png";
+import dashboard from "@/public/Dashboard.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Personal portfolio",
+    description:
+      "Discover my latest portfolio, crafted with Next.js and TypeScript. Experience a blend of speed, elegance, and innovation in web design.",
+    tags: ["Next.js", "Typescript", "Tailwind CSS", "Framer Motion"],
+    imageUrl: personalPortfolio,
+  },
+  {
+    title: "Shopping Hub",
+    description:
+      "Experience the future of shopping on our e-commerce platform. Dive into curated collections. The world's marketplace, now just a click away.",
+    tags: ["React", "Redux toolkit", "React router dom", "hooks"],
+    imageUrl: shoppingHub,
+  },
+  {
+    title: "Foode",
+    description:
+      "Discover your next meal effortlessly with our Recipe Finder! Choose a cuisine, and we'll serve up the perfect recipe. Taste the world in a click",
+    tags: ["React", "Next.js", "Tailwind"],
+    imageUrl: foodeRecipeFinder,
+  },
+  {
+    title: "Lavish",
+    description:
+      "Experience banking redefined on our new landing page! With intuitive design and effortless navigation, managing your finances is a click away.",
+    tags: ["React", "React router dom", "hooks"],
+    imageUrl: banking,
+  },
+  {
+    title: "Dashboard",
+    description:
+      "Dive into our new dashboard page: sleek, intuitive, and designed for ultimate control. Efficient web interaction, now just a click away.",
+    tags: ["HTML5", "CSS3", "CSS animation"],
+    imageUrl: dashboard,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Git",
+  "Tailwind",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Framer Motion",
+] as const;
