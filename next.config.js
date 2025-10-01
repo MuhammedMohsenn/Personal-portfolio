14414d86fb053f6fa730ae559d9d74122ce112fb
MuhammedMohsenn <muhammedmohsen8695@gmail.com>
2025-10-01 12:36:26 +0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    loader: "default",
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        loader: "default",
       },
     ],
   },
